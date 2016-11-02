175b8118a2a322f491ff8b07fc72ebb2d6fbab56
X1aoboo <807266405@qq.com>
2016-11-02 22:57:07 +0800

    第三次修改

--- first.cpp
+++ first.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
-    cout << “Hello,World!” <<endl;
+    int a, b;
+    cin >> a >> b;
+    a += b;
+    cout << “Hello,World!\n” << a << endl;
     return 0;
 }
